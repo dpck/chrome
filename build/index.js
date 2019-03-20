43d273f6f95ff41726d2fab594c25f2dbfc6e8b5
Anton <anton@adc.sh>
2019-03-20 17:02:35 +0300

    add src

--- build/index.js
+++ build/index.js
@@ -1,29 +1,77 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('@contexts/chrome')
+let CDP = require('chrome-remote-interface'); if (CDP && CDP.__esModule) CDP = CDP.default;
 
 /**
- * The Remote Chrome Context For Testing Like Webdriver.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * The context for testing in headless Chrome. Connects to the remote instance and enables the page for testing.
+ * https://chromedevtools.github.io/devtools-protocol
  */
-               async function chrome(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@contexts/chrome called with %s', text)
-  return text
+               class RemoteChrome {
+  async _init() {
+    const client = await CDP({
+      host: '127.0.0.1', // how to configure?
+      port: '9222',
+    })
+    const { Network, Page, Runtime } = client
+    await Network.enable()
+    await Page.enable()
+    this.client = client
+    this._Page = Page
+    this._Runtime = Runtime
+    this.Network = Network
+    // console.log('[%s]: %s', c('RemoteChrome', 'red'), b('Page enabled', 'green'))
+  }
+  static get _timeout() {
+    return 10000
+  }
+  /**
+   * The enabled page.
+   */
+  get Page() {
+    return this._Page
+  }
+  /**
+   * The runtime.
+   */
+  get Runtime() {
+    return this._Runtime
+  }
+  async _destroy() {
+    if (this.client) {
+      await this.client.close()
+    }
+  }
+  /**
+   * Evaluates the expression and returns the result. Throws an error if it was present.
+   * @param {string} expression
+   * @param {boolean} [json=true] Use JSON serialisation for data. If set to false, just the result is returned.
+   */
+  async evaluate(expression, json = true) {
+    const e = json ? `JSON.stringify(${expression}, null, 2)` : expression
+    const res = await this.Runtime.evaluate({ expression: e })
+    const { exceptionDetails } = res
+    if (exceptionDetails) {
+      throw new Error(exceptionDetails.exception.description)
+    }
+    if (!json) {
+      return res.result
+    }
+    const { value, type } = res.result
+    if (type == 'undefined') return undefined
+    const val = JSON.parse(value)
+    return val
+  }
+  /**
+   * Takes a screenshot and returns a buffer.
+   * @param {string} [selector] An optional selector to get the bounding box to capture.
+   */
+  async takeScreenshot(selector) {
+    let rect = {}
+    if (selector) rect = await this.evaluate(`document.body.querySelector('${selector}').getBoundingClientRect()`)
+    const { data } = await this.Page.captureScreenshot({
+      clip: { ...rect, scale: 1 },
+    })
+    const screenshot = new Buffer(data, 'base64')
+    return screenshot
+  }
 }
 
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
-
-
-module.exports = chrome
+module.exports = RemoteChrome
